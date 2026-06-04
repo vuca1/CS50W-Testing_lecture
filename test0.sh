29fe8284b52e6187f2e9961b8720ae48225bf423
vuca1 <malihavita@gmail.com>
2026-06-04 08:05:31 +0200

    Add prime number checking function and corresponding tests

---
+++ test0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python -c "from test0 import test_prime; test_prime(1, False)"
+python -c "from test0 import test_prime; test_prime(2, True)"
+python -c "from test0 import test_prime; test_prime(8, False)"
+python -c "from test0 import test_prime; test_prime(11, True)"
+python -c "from test0 import test_prime; test_prime(25, False)"
+python -c "from test0 import test_prime; test_prime(28, False)"

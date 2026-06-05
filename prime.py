d69b74b15d710d7a6282ce4826f2aefe6b4c6739
vuca1 <malihavita@gmail.com>
2026-06-05 13:16:08 +0200

    added airline project

--- prime.py
+++ prime.py
@@ -1,7 +1,9 @@
+import math
+
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
